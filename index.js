77da2bf1d7f6b18053b8d480d802ce1468d57405
John Nash <redd@redd.ink>
2021-10-12 15:15:18 +1100

    add some logging

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const CoinGecko = require('coingecko-api');
 const cache = require('memory-cache');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('combined'));
 const port = 3001;
 const CoinGeckoClient = new CoinGecko();
 
